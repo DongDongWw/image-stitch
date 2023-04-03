86c658e6d56097cd0fc79995bae667f8fa99ec6e
DongDongWw <276640089@qq.com>
2023-04-03 19:49:03 +0800

    add gaussian filter to harris_self.cpp

--- code/harris_self.cpp
+++ code/harris_self.cpp
@@ -45,8 +45,7 @@ void harris_self(const Mat& img, int num_of_corner, vector<KeyPoint>& kp_vec) {
     }
 
     // ******************* structure tensor *******************
-    // repeat the edge pixels
-    // x
+    // x padding
     Eigen::MatrixXi padded_drt_x_e(img_e.rows()+2, img_e.cols()+2);
     padded_drt_x_e.block(1, 1, img_e.rows(), img_e.cols()) = drt_x_e;
     padded_drt_x_e(0, 0) = 0;
@@ -57,8 +56,7 @@ void harris_self(const Mat& img, int num_of_corner, vector<KeyPoint>& kp_vec) {
     padded_drt_x_e.block(0, 1, 1, img_e.cols()) = drt_x_e.row(0);
     padded_drt_x_e.block(1, img_e.cols()+1, img_e.rows(), 1) = drt_x_e.col(img_e.cols()-1);
     padded_drt_x_e.block(img_e.rows()+1, 1, 1, img_e.cols()) = drt_x_e.row(img_e.rows()-1);
-    
-    // y
+    // y padding
     Eigen::MatrixXi padded_drt_y_e(img_e.rows()+2, img_e.cols()+2);
     padded_drt_y_e.block(1, 1, img_e.rows(), img_e.cols()) = drt_y_e;
     padded_drt_y_e(0, 0) = 0;
@@ -70,49 +68,56 @@ void harris_self(const Mat& img, int num_of_corner, vector<KeyPoint>& kp_vec) {
     padded_drt_y_e.block(1, img_e.cols()+1, img_e.rows(), 1) = drt_y_e.col(img_e.cols()-1);
     padded_drt_y_e.block(img_e.rows()+1, 1, 1, img_e.cols()) = drt_y_e.row(img_e.rows()-1);
     
-    // calculate R without store the structure tensor
-    int blocksize = 3;
-    float k = 0.04;
-    Eigen::MatrixXd R(img_e.rows(), img_e.cols());
-
-    for(int i=1; i<img_e.rows()+1; ++i) {
-        for(int j=1; j<img_e.cols()+1; ++j) {
-            int squr_sum_x = 0,
-                squr_sum_y = 0,
-                sum_xy = 0;
-            for(int k=0; k<blocksize; ++k) {
-                for(int l=0; l<blocksize; ++l) {
-                    squr_sum_x += padded_drt_x_e(i-1+k, j-1+l) * padded_drt_x_e(i-1+k, j-1+l);
-                    squr_sum_y += padded_drt_y_e(i-1+k, j-1+l) * padded_drt_y_e(i-1+k, j-1+l);
-                    sum_xy += padded_drt_x_e(i-1+k, j-1+l) * padded_drt_y_e(i-1+k, j-1+l);
-                }
-            }
-
-            Eigen::Matrix2d temp;
-            double r;
-
-            temp << squr_sum_x, sum_xy,
-                    sum_xy, squr_sum_y;
-            r = temp.determinant()-k*(temp.trace()*temp.trace());
-            R(i-1, j-1) = r;
+    Eigen::MatrixXf padded_drt_x2_e = padded_drt_x_e.cwiseProduct(padded_drt_x_e);
+    Eigen::MatrixXf padded_drt_y2_e = padded_drt_y_e.cwiseProduct(padded_drt_y_e);
+    Eigen::MatrixXf padded_drt_xy_e = padded_drt_x_e.cwiseProduct(padded_drt_y_e);
+    // gaussion window
+    float sigma = 2;
+    int blocksize = 3*sigma+1;
+    Eigen::MatrixXf guassion_filter(blocksize, blocksize);
+    for(int i=0; i<blocksize; ++i) {
+        for(int j=0; j<blocksize; ++j) {
+            int offset = (blocksize-1)/2;
+            int x2 = (i-offset)*(i-offset);
+            int y2 = (j-offset)*(j-offset);
+            guassion_filter(i, j) = exp(-(x2+y2)/(2*M_PI*sigma*sigma));
         }
     }
-    cout << R.row(0) << endl;
+    // R matrix
+    Eigen::MatrixXf R(img_e.rows(), img_e.cols());
+    float k = 0.04;
+    for(int i=1; i<img_e.rows()+1; ++i) {
+        for(int j=1; j<img_e.cols()+1; ++j) {
+            Eigen::MatrixXf temp_x2 = padded_drt_x2_e.block(i-1, j-1, blocksize, blocksize);
+            Eigen::MatrixXf temp_y2 = padded_drt_y2_e.block(i-1, j-1, blocksize, blocksize);
+            Eigen::MatrixXf temp_xy = padded_drt_xy_e.block(i-1, j-1, blocksize, blocksize);
+            Eigen::MatrixXf temp_x2_g = temp_x2.cwiseProduct(guassion_filter);
+            Eigen::MatrixXf temp_y2_g = temp_y2.cwiseProduct(guassion_filter);
+            Eigen::MatrixXf temp_xy_g = temp_xy.cwiseProduct(guassion_filter);
+            float x2_sum = temp_x2_g.sum();
+            float y2_sum = temp_y2_g.sum();
+            float xy_sum = temp_xy_g.sum();
+            Eigen::Matrix2f temp;
+            temp << x2_sum, xy_sum, xy_sum, y2_sum;
+            float R_value = temp.determinant() - k*temp.trace()*temp.trace();
+            R(i-1, j-1) = R_value;
+        }
+    }
 
     // ******************* non-maximum suppression *******************
-    Eigen::MatrixXd R_nms(img_e.rows(), img_e.cols());
+    Eigen::MatrixXf R_nms(img_e.rows(), img_e.cols());
     R_nms = R;
-    R_nms.block(0, 0, 1, img_e.cols()) = Eigen::MatrixXd::Zero(1, img_e.cols());
-    R_nms.block(img_e.rows()-1, 0, 1, img_e.cols()) = Eigen::MatrixXd::Zero(1, img_e.cols());
-    R_nms.block(0, 0, img_e.rows(), 1) = Eigen::MatrixXd::Zero(img_e.rows(), 1);
-    R_nms.block(0, img_e.cols()-1, img_e.rows(), 1) = Eigen::MatrixXd::Zero(img_e.rows(), 1);
+    R_nms.block(0, 0, 1, img_e.cols()) = Eigen::MatrixXf::Zero(1, img_e.cols());
+    R_nms.block(img_e.rows()-1, 0, 1, img_e.cols()) = Eigen::MatrixXf::Zero(1, img_e.cols());
+    R_nms.block(0, 0, img_e.rows(), 1) = Eigen::MatrixXf::Zero(img_e.rows(), 1);
+    R_nms.block(0, img_e.cols()-1, img_e.rows(), 1) = Eigen::MatrixXf::Zero(img_e.rows(), 1);
     
     // !! map不能自定义按值排序!!所以用vector<pair<pair<int, int>, double>>
     vector<pair<pair<int, int>, double>> sort_map;
     
     for(int i=1; i<img_e.rows()-1; ++i) {
         for(int j=1; j<img_e.cols()-1; ++j) {
-            double r = R(i, j);
+            float r = R(i, j);
             if(r < R(i-1, j-1) || r < R(i-1, j) || r < R(i-1, j+1) ||
                r < R(i, j-1) || r < R(i, j+1) ||
                r < R(i+1, j-1) || r < R(i+1, j) || r < R(i+1, j+1)) {
@@ -120,6 +125,7 @@ void harris_self(const Mat& img, int num_of_corner, vector<KeyPoint>& kp_vec) {
             }
             else {
                 sort_map.push_back(make_pair(make_pair(i-1, j-1), r));
+                cout << r << endl;
             }
         }
     }
